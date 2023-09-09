6b10e21087070dd2c4619dd2fbafd59c50df8cb8
Vladyslav <dr.tienam123@gmail.com>
2023-09-09 07:11:20 +0300

    add description

--- practice.php
+++ practice.php
@@ -50,49 +50,52 @@ var_dump( preg_replace( '#ab*a#', 'slovo', $str ) );
 В следующем примере шаблон поиска выглядит так: буква 'x', далее строка 'ab' один или более раз, потом буква 'x':*/
 
 $str = 'xabx xababx xaabbx';
-$res = preg_replace( '#x(ab)+x#', 'slovo', $str);
-var_dump( $res);
+$res = preg_replace( '#x(ab)+x#', 'slovo', $str );
+var_dump( $res );
 echo "<hr>";
 
 $str = 'a+x ax aax aaax';
-$res = preg_replace( '#a\+x#', 'slovo', $str);
-var_dump( $res);
+$res = preg_replace( '#a\+x#', 'slovo', $str );
+var_dump( $res );
 echo "<hr>";
 
 
 //Напишите регулярку, которая найдет строку 'a.a', не захватив остальные.
 $str = 'a.a aba aea';
-$res = preg_replace( '#a\.a#', 'changed', $str);
-var_dump( $res);
+$res = preg_replace( '#a\.a#', 'changed', $str );
+var_dump( $res );
 echo "<hr>";
 
 //Напишите регулярку, которая найдет строку '2+3', не захватив остальные.
 $str = '2+3 223 2223';
-$str = preg_replace( '#2\+3#', 'changed', $str);
-var_dump( $res);
+$str = preg_replace( '#2\+3#', 'changed', $str );
+var_dump( $res );
 echo "<hr>";
 
 //Напишите регулярку, которая найдет строки '2+3', '2++3', '2+++3', не захватив остальные (+ может быть любое количество)
 $str = '23 2+3 2++3 2+++3 345 567';
-$res = preg_replace( '#2\++3#', 'changed', $str);
-var_dump( $res);
+$res = preg_replace( '#2\++3#', 'changed', $str );
+var_dump( $res );
 echo "<hr>";
 
 //Напишите регулярку, которая найдет строки '23', '2+3', '2++3', '2+++3', не захватив остальные.
 $str = '23 2+3 2++3 2+++3 445 677';
-$res = preg_replace( '#2\+*3#', 'slovo', $str);
-var_dump( $res);
+$res = preg_replace( '#2\+*3#', 'slovo', $str );
+var_dump( $res );
 echo "<hr>";
 
 
 //Напишите регулярку, которая найдет строки '*q+', '*qq+', '*qqq+', не захватив остальные.
 $str = '*+ *q+ *qq+ *qqq+ *qqq qqq+';
-$res = preg_replace( '#\*q+\+#', 'Tienam', $str);
-var_dump( $res);
+$res = preg_replace( '#\*q+\+#', 'Tienam', $str );
+var_dump( $res );
 echo "<hr>";
 
 //Напишите регулярку, которая найдет строки в квадратных скобках и заменят их на '!'.
 $str = '[abc] {abc} abc (abc) [abc]';
-$res = preg_replace( '#\[\w{3}\]#', 'Tienam', $str);
-var_dump( $res);
+$res = preg_replace( '#\[\w{3}\]#', 'Tienam', $str );
+var_dump( $res );
 echo "<hr>";
+
+/*Операторы +, *, ? хороши, однако, с их помощью нельзя указать конкретное число повторений. В этом случае вам на помощь придет оператор {}.
+Работает он следующим образом: {5} - пять повторений, {2,5} - повторяется от двух до пяти (оба включительно), {2,} - повторяется два и более раз.*/
