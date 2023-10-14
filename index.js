256b980e15b55cbb2f5ba20478d15c785358d43d
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-10-14 15:20:35 +0300

    comolete the task 469

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-const button = document.querySelector('.button');
-const title = document.querySelector('.title');
+const list = document.querySelector('#table');
+let users = [
+  {
+    name: 'name',
+    age: 'age',
+    salary: 'salary',
+  },
+  {name: 'employee1', age: 30, salary: 400},
+  {name: 'employee2', age: 31, salary: 500},
+  {name: 'employee3', age: 32, salary: 600},
+];
+for (let i = 0; i < users.length; i++) {
+  const tr = document.createElement('tr');
+  for (const userKey in users[i]) {
+    if (i === 0) {
+      const th = document.createElement('th');
+      th.textContent = users[i][userKey];
+      th.style.padding = '10px';
+      tr.append(th);
+    } else {
+      const td = document.createElement('td');
+      td.setAttribute('value', userKey);
+      td.style.padding = '10px';
+      td.style.outline = '1px solid red';
+      td.textContent = users[i][userKey];
+      tr.append(td);
+      if (td.getAttribute('value') === 'age') {
 
-function timer() {
-  let i = 0;
-  let intervalId = setInterval(
-      () => {
-        let date = new Date();
-        title.innerHTML = date.getHours() + ':' + date.getMinutes() + ':' +
-            date.getSeconds();
-      }, 1000,
-  );
+      }
+    }
+  }
+  list.append(tr);
 }
+list.style.textAlign = 'center';
 
-button.addEventListener('click', function func() {
-  timer();
-  this.removeEventListener('click', func);
+list.addEventListener('click', (e) => {
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  if (e.target.getAttribute('value') === 'age') {
+    let value = +e.target.textContent;
+    e.target.innerHTML = value + 1;
+  }
+  if (e.target.getAttribute('value') === 'salary') {
+    let salary = e.target.textContent * 0.1;
+    e.target.innerHTML = Math.round(+e.target.textContent + salary);
+  }
 });
