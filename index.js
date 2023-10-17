7247139f222b1e89c29ffa3fcbfcc84eb0b63b91
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-10-18 00:53:58 +0300

    Игра крестики нолики

--- index.js
+++ index.js
@@ -1,59 +1,117 @@
-import auto from "./recipes.js";
-// Определяем DOM елементы
-const list = document.querySelector('.cards');
+// import auto from "./recipes.js";
+// // Определяем DOM елементы
+// const list = document.querySelector('.cards');
+//
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <div class="container-image" style="width: 100%;height: 80%;background: #fff;">
+//         <img src="" alt="">
+//         <div class="container-text" style="width: 100%;height: 20%;background: #fff;">
+//         <img src="" alt="">
+//          <h3 class="title">A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</h3>
+//     </div>
+// `, {
+//     closable: false,
+//     onShow: (instance) => {
+//         instance.element().addEventListener('click', onCloseCard)
+//         window.addEventListener('keydown', onPressKey)
+//     },
+// })
+// // Определяем разметку
+// const markup = auto.map(el => {
+//     return `<li class="js-target" data-car-url="${el.image}" data-car-title="${el.name}">
+// <img class="js-target"  src="${el.image}" width="300" height="200">
+// <h3 class="js-target">${el.name}</h3>
+// <p class="js-target">${el.price}</p>
+// </li>`
+// }).join('');
+// list.insertAdjacentHTML('beforeend', markup)
+// // Делегируем события по клику
+// list.addEventListener('click', onClickCard);
+//
+// function onClickCard(event) {
+//     const {image, id} = auto;
+//     if (event.target == this) {
+//         return;
+//     }
+//     const item = event.target.dataset.carUrl ?? event.target.closest('li').dataset.carUrl;
+//     instance.show(() => {
+//         instance.element().querySelector('img').src = '';
+//         instance.element().querySelector('img').src = item;
+//         instance.element().querySelector('h3.title').textContent = event.target.dataset.carTitle ?? event.target.closest('li').dataset.carTitle;
+//     });
+// }
+//
+// // Добавляем событие на клик в любую часть екрана
+// function onCloseCard(e) {
+//     instance.close()
+//     removeEventListener("click", onCloseCard)
+// }
+//
+// // Добавляем событие на клик по кнопке Esc
+// function onPressKey(e) {
+//     if (e.code === 'Escape') {
+//         instance.close();
+//         removeEventListener('keydown', onPressKey)
+//     }
+// }
+//
+// console.log(window)
+// console.log(_.trim)
 
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <div class="container-image" style="width: 100%;height: 80%;background: #fff;">
-        <img src="" alt="">
-        <div class="container-text" style="width: 100%;height: 20%;background: #fff;">
-        <img src="" alt="">
-         <h3 class="title">A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</h3>
-    </div>
-`, {
-    closable: false,
-    onShow: (instance) => {
-        instance.element().addEventListener('click', onCloseCard)
-        window.addEventListener('keydown', onPressKey)
-    },
-})
-// Определяем разметку
-const markup = auto.map(el => {
-    return `<li class="js-target" data-car-url="${el.image}" data-car-title="${el.name}">
-<img class="js-target"  src="${el.image}" width="300" height="200">
-<h3 class="js-target">${el.name}</h3>
-<p class="js-target">${el.price}</p>
-</li>`
-}).join('');
-list.insertAdjacentHTML('beforeend', markup)
-// Делегируем события по клику
-list.addEventListener('click', onClickCard);
+const products = [
+  {
+    id: 'sku1', quantity: 1,
+  },
+  {
+    id: 'sku2', quantity: 2,
+  },
+  {
+    id: 'sku3', quantity: 3,
+  },
+  {
+    id: 'sku1', quantity: 6,
+  },
+  {
+    id: 'sku1', quantity: 8,
+  },
+  {
+    id: 'sku2', quantity: 19,
+  },
+  {
+    id: 'sku4', quantity: 1,
+  },
+];
 
-function onClickCard(event) {
-    const {image, id} = auto;
-    if (event.target == this) {
-        return;
+/*function sortProducts(arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = i + 1; j < arr.length; j += 1) {
+      if (arr[i].id === arr[j].id) {
+        arr[i].quantity += arr[j].quantity;
+        arr.splice(j, 1);
+        j -= 1;
+      }
     }
-    const item = event.target.dataset.carUrl ?? event.target.closest('li').dataset.carUrl;
-    instance.show(() => {
-        instance.element().querySelector('img').src = '';
-        instance.element().querySelector('img').src = item;
-        instance.element().querySelector('h3.title').textContent = event.target.dataset.carTitle ?? event.target.closest('li').dataset.carTitle;
-    });
+
+  }
+  console.log(arr);
 }
 
-// Добавляем событие на клик в любую часть екрана
-function onCloseCard(e) {
-    instance.close()
-    removeEventListener("click", onCloseCard)
-}
+sortProducts(products);*/
+const idToQuantity = {};
+products.forEach(product => {
+  const {id, quantity} = product;
+  if (idToQuantity[id]) {
+    idToQuantity[id] += quantity;
+  } else {
+    idToQuantity[id] = quantity;
+  }
+});
 
-// Добавляем событие на клик по кнопке Esc
-function onPressKey(e) {
-    if (e.code === 'Escape') {
-        instance.close();
-        removeEventListener('keydown', onPressKey)
-    }
-}
+// Преобразовываем объект обратно в массив без дубликатов
+const result = Object.keys(idToQuantity).map(id => ({
+  id,
+  quantity: idToQuantity[id],
+}));
 
-console.log(_.throttle)
+console.log(result);
