eedd93fe05e2297be98a249cd57ab9014cd81901
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-11-12 14:31:11 +0200

    Complete task eventListener

--- events.js
+++ events.js
@@ -1,13 +1,27 @@
-// click
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+class Rect {
+  constructor(width, height) {
+    // Конструктор - инициализирует екземпляр класса Rect представляет обычную функцию констрруктор
+    this.width = width;
+    this.height = height;
+  }
+
+  // метод , будет доабвлен в прототип Rect.prototype
+  getArea() {
+    return this.width * this.height;
+  }
+
+  getPerimeter() {
+    return this.height * 2 + this.width * 2;
+  }
 }
 
-document.querySelector('#testForm').addEventListener('reset', function(event) {
-  alert('Обработчик события reset');
-  let result = confirm('Очистить форму?');
-  if (!result) {
-    event.preventDefault();
-    event.stopPropagation();
-  }
-});
+let r1 = new Rect(10, 20);
+let r2 = new Rect(15, 9);
+
+console.log('r1 area ' + r1.getArea());
+console.log('r1 perimeter ' + r1.getPerimeter());
+
+console.log('r2 area ' + r2.getArea());
+console.log('r2 perimeter ' + r2.getPerimeter());
+console.log(r1);
+
