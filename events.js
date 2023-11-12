003ba9edac6dfb3d5c3cdb3866e5b9cfb1e50246
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-11-12 14:40:51 +0200

    Complete task getter the Class

--- events.js
+++ events.js
@@ -1,4 +1,4 @@
-class Rect {
+/*class Rect {
   constructor(width, height) {
     // Конструктор - инициализирует екземпляр класса Rect представляет обычную функцию констрруктор
     this.width = width;
@@ -13,8 +13,8 @@ class Rect {
   getPerimeter() {
     return this.height * 2 + this.width * 2;
   }
-}
-
+}*/
+/*
 let r1 = new Rect(10, 20);
 let r2 = new Rect(15, 9);
 
@@ -24,4 +24,36 @@ console.log('r1 perimeter ' + r1.getPerimeter());
 console.log('r2 area ' + r2.getArea());
 console.log('r2 perimeter ' + r2.getPerimeter());
 console.log(r1);
+*/
+/*
+class Rect {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  //getter - синтаксис связывавющий свойство обьекта с методом
+  //при обращениии к obj.width будет вызвана функция get width();
+
+  get width() {
+    console.log('get width()');
+    return this._width;
+  }
+
+  get height() {
+    console.log('get height()');
+    return this._height;
+  }
+
+  get area() {
+    console.log('get area()');
+    return this._height * this._width;
+  }
+}
+
+let r1 = new Rect(10, 20);
+console.log('width ' + r1.width);
+console.log('height ' + r1.height);
+console.log('area ' + r1.area);
+*/
 
